af336e4842fe2daf495862aa0145d445c42a375a
Cansua <cansuarar1@gmail.com>
2024-09-19 16:16:21 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanySoftware
+//
+//  Created by CANSU ARAR on 19.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
